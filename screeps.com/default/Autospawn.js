bcba282c6a4f0ffef6351c1603d02180e434eacd
JoshuaG <joshuajamesgrant02@gmail.com>
2017-08-22 14:23:03 -0500

    Deleted tutorial, Made Autospawn category

---
+++ screeps.com/default/Autospawn.js
@@ -0,0 +1,67 @@
+
+module.exports = {
+
+    run: function() {
+
+        //Clear Memory
+
+        for(var name in Memory.creeps) {
+            if(!Game.creeps[name]) {
+                delete Memory.creeps[name];
+                console.log('Clearing non-existing creep memory:', name);
+            }
+        }
+        
+        //Count Harvesters
+        
+        var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+        console.log('Harvesters: ' + harvesters.length);
+        
+        //If Harvesters < 3, Spawn more
+        
+        if(harvesters.length < 3) {
+
+            //Spawn Creeps with less parts if Creeps die
+
+            if(harvesters.lenght == 0){var newName = Game.spawns['JoshSpawn'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'harvester'});
+                console.log('Spawning new harvester: ' + newName);
+            }
+                
+            //Spawn Creeps with more parts if I have more creeps
+
+            else {
+                var newName = Game.spawns['JoshSpawn'].createCreep([WORK, WORK, CARRY, MOVE, MOVE], undefined, {role: 'harvester'});
+                console.log('Spawning new harvester: ' + newName);
+            }        
+        }
+
+        //Prioritize Harvesters
+        
+        else {
+        
+            //Count Upgraders
+        
+            var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+            console.log('Upgraders: ' + upgraders.length);
+        
+            //If Upgraders < 15, Spawn more
+        
+            if(upgraders.length < 12) {
+                var newName = Game.spawns['JoshSpawn'].createCreep([WORK, WORK, CARRY, MOVE, MOVE], undefined, {role: 'upgrader'});
+                console.log('Spawning new upgrader: ' + newName);
+            }
+        
+            //Count Builders
+        
+            var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+                console.log('Builders: ' + builders.length);
+            
+            //If Builders < 3, Spawn more
+            
+            if(builders.length < 3) {
+                var newName = Game.spawns['JoshSpawn'].createCreep([WORK, WORK, CARRY, MOVE, MOVE], undefined, {role: 'builder'});
+                console.log('Spawning new builder: ' + newName);
+            }
+        }
+    }
+};
--- screeps.com/default/main.js
+++ screeps.com/default/main.js
@@ -1,54 +1,13 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
-
+var Autospawn = require('Autospawn');
 module.exports.loop = function () {
 
-    for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
-        }
-    }
-
-    //Count Harvesters
-
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
-
-    //If Harvesters < 3, Spawn more
-
-    if(harvesters.length < 3) {
-        var newName = Game.spawns['JoshSpawn'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
-        console.log('Spawning new harvester: ' + newName);
-    }
-    //Prioritize Harvesters
-    else {
-
-        //Count Upgraders
-
-        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-        console.log('Upgraders: ' + upgraders.length);
-
-        //If Upgraders < 15, Spawn more
-
-        if(upgraders.length < 15) {
-            var newName = Game.spawns['JoshSpawn'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
-            console.log('Spawning new upgrader: ' + newName);
-        }
-
-        //Count Builders
-
-        var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-        console.log('Builders: ' + builders.length);
     
-        //If Builders < 3, Spawn more
+    Autospawn.run();
+
     
-        if(builders.length < 3) {
-            var newName = Game.spawns['JoshSpawn'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
-            console.log('Spawning new builder: ' + newName);
-        }
-    }
     
     if(Game.spawns['JoshSpawn'].spawning) { 
         var spawningCreep = Game.creeps[Game.spawns['JoshSpawn'].spawning.name];
